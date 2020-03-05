7318301a790adeb3f78be32cfe19153d199cc712
Gabriel Kasten <Gabriel@MacBook-Pro-de-Gabriel-2.local>
2020-03-05 16:35:27 -0600

    arduino

--- sketch_mar04a/sketch_mar04a.ino
+++ sketch_mar04a/sketch_mar04a.ino
@@ -3,9 +3,7 @@ int analogCapteur = A0;
 
 double Thermistor(int RawADC){
     double Temp;
-     Temp = log(((10240000/RawADC) - 10000));
-    Temp = 1 / (0.001129148 + (0.000234125 * Temp) + (0.0000000876741 * Temp * Temp * Temp));
-    Temp = Temp - 273.15;           // Convert Kelvin to Celcius
+    Temp = RawADC / 15.5;
     return Temp;
 }
 
